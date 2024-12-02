fc15b593789f651149ed7fc254d6b70f6e349b07
sage <sage.b.shaw@gmail.com>
2024-12-02 00:03:49 -0700

    day 2, part 2

--- day2/day2.py
+++ day2/day2.py
@@ -22,7 +22,7 @@ def small_difference(report: Report) -> bool:
     return all(1 <= abs(a - b) <= 3 for a, b in pairwise(report))
 
 
-def is_safe(report: Report):
+def is_safe(report: Report) -> bool:
     return small_difference(report) and (is_increasing(report) or is_decreasing(report))
 
 
@@ -31,7 +31,20 @@ def part_one(reports: list[Report]) -> int:
     return num_safe
 
 
+def drop_one(report) -> list[Report]:
+    return [report[:index] + report[index + 1 :] for index in range(len(report))]
+
+
+def is_mostly_safe(report) -> bool:
+    return any(is_safe(fixed_report) for fixed_report in drop_one(report))
+
+
+def part_two(reports: list[Report]) -> int:
+    num_safe = sum(is_mostly_safe(report) for report in reports)
+    return num_safe
+
+
 if __name__ == "__main__":
     reports = load_reports()
-    num_safe = part_one(reports)
-    print(f"{num_safe=}")
+    print(f"part_one: num_safe={part_one(reports)}")
+    print(f"part_two: num_safe={part_two(reports)}")
